10347fdecc94f6fbdfa7a96bdf42ed6328f8e9e5
Sharukh Khan <enetsharukh@gmail.com>
2025-06-22 23:42:56 +0530

    hoisting questions added.

--- Hoisting/index.js
+++ Hoisting/index.js
@@ -13,3 +13,16 @@
   let result =test();
   console.log(result)
   
+
+// Overshadow variable Example 1
+  var foo = 'LION';
+
+function showName(){
+    foo = 'PANDA';
+    return;
+    function foo(){};
+};
+let res=showName();  // Guess o/p
+console.log(res)
+console.log(foo); // Guess o/p
+
