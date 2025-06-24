063cb30cac9450632cb1a30ad88f286e92036d42
Sharukh Khan <enetsharukh@gmail.com>
2025-06-24 23:23:36 +0530

    question added for method hoisting.

--- Hoisting/index.js
+++ Hoisting/index.js
@@ -26,3 +26,22 @@ let res=showName();  // Guess o/p
 console.log(res)
 console.log(foo); // Guess o/p
 
+
+
+// Example 4: Methods Hoisting
+
+
+console.log(myObjA.sayHello()); // Guess the O/p
+console.log(myObjB.sayHello()); // Guess the O/p
+
+var myObjA = {
+  sayHello: function () {
+    return "Hello!";
+  }
+};
+
+const myObjB = {
+  sayHello: function () {
+    return "Hello!";
+  }
+};
